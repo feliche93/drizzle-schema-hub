49ce32eef0de17352193c467f9e2b2c194bd69f9
feliche93 <felix.vemmer@gmail.com>
2023-10-12 16:00:57 +0200

    chore: linting config

--- .prettier.config.js
+++ .prettier.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   semi: false,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: "all",
   printWidth: 100,
   tabWidth: 2,
   useTabs: false,
-  plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
   importOrder: [
-    '^((react|next)/(.*)$)|^((react|next)$)',
-    '<THIRD_PARTY_MODULES>',
-    '^@(config|types|styles|utils|hooks|components|app|pages|features)/(.*)$',
-    '^[./]',
+    "^((react|next)/(.*)$)|^((react|next)$)",
+    "<THIRD_PARTY_MODULES>",
+    "^@(config|types|styles|utils|hooks|components|app|pages|features)/(.*)$",
+    "^[./]",
   ],
   importOrderSeparation: true,
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ["*.ts", "*.tsx"],
       options: {
-        parser: 'typescript',
-        importOrderParserPlugins: ['typescript', 'jsx'],
+        parser: "typescript",
+        importOrderParserPlugins: ["typescript", "jsx"],
       },
     },
   ],
-}
+};
